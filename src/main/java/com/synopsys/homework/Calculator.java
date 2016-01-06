1337cd72bf2af412d8501b1ea46e7761a989a585
dchupilko <denys.chupilko@sjrb.ca>
2016-01-06 11:51:19 +0200

    add log4j support

--- src/main/java/com/synopsys/homework/Calculator.java
+++ src/main/java/com/synopsys/homework/Calculator.java
@@ -1,6 +1,10 @@
 package com.synopsys.homework;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class Calculator {
+    static final Logger log = LogManager.getLogger(Calculator.class.getName());
 
     public static void main(String[] args) {
 
