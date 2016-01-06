4caccff890c2ca958303ebcb2304e8414df85e1e
dchupilko <denys.chupilko@sjrb.ca>
2016-01-07 00:37:18 +0200

    add command line input

--- src/main/java/com/synopsys/homework/Calculator.java
+++ src/main/java/com/synopsys/homework/Calculator.java
@@ -1,5 +1,6 @@
 package com.synopsys.homework;
 
+import com.synopsys.homework.compiler.Compiler;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -7,6 +8,21 @@ public class Calculator {
     static final Logger log = LogManager.getLogger(Calculator.class.getName());
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Usage: <expression>");
+            System.exit(1);
+        }
 
+        String expression = args[0];
+
+        try {
+            Compiler compiler = new Compiler();
+            int result = compiler.compile(expression);
+            System.out.println(result);
+        } catch (RuntimeException e) {
+            log.error("Error during calculating expression \"" + expression + "\"", e);
+            System.err.println("Unable to calculate expression \"" + args[0] + "\". See log for details.");
+            System.exit(1);
+        }
     }
 }
